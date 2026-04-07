d18715c5160a609d1e55dbf225db218a38fe60a2
Kalp1606 <121572403+kalpprajapati@users.noreply.github.com>
2026-04-07 19:01:19 +0530

    Add solution for second-largest-element

---
+++ second-largest-element/Solution.java
@@ -0,0 +1,48 @@
+class Solution {
+
+    // Function to find the second smallest element in the array
+    public static int secondSmallest(int[] arr, int n) {
+        // Edge case: if the array has fewer than 2 elements
+        if (n < 2)
+            return -1;
+
+        int small = Integer.MAX_VALUE;
+        int second_small = Integer.MAX_VALUE;
+
+        // Loop through the array to find the second smallest element
+        for (int i = 0; i < n; i++) {
+           
+            if (arr[i] < small) {
+                second_small = small;
+                small = arr[i];
+            } 
+            else if (arr[i] < second_small && arr[i] != small) {
+                second_small = arr[i];
+            }
+        }
+        return second_small;
+    }
+
+    // Function to find the second largest element in the array
+    public static int secondLargest(int[] arr, int n) {
+        // Edge case: if the array has fewer than 2 elements
+        if (n < 2)
+            return -1;
+
+        int large = Integer.MIN_VALUE, second_large = Integer.MIN_VALUE;
+
+        // Loop through the array to find the second largest element
+        for (int i = 0; i < n; i++) {
+            
+            if (arr[i] > large) {
+                second_large = large;
+                large = arr[i];
+            } 
+            else if (arr[i] > second_large && arr[i] != large) {
+                second_large = arr[i];
+            }
+        }
+        return second_large; 
+    }
+}
+
