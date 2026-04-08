1f5d335928a86aa6f40d07c7c4609612c42bd586
Kalp1606 <121572403+kalpprajapati@users.noreply.github.com>
2026-04-08 21:19:13 +0530

    Add Java solution - Remove Duplicates

---
+++ 26. remove-duplicates-from-sorted-array/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    // Function to remove duplicates from sorted array in-place
+    public int removeDuplicates(int[] nums) {
+        if(nums.length==0){
+            return 0;
+        }
+        int i=0;
+        for(int j=1;j<nums.length;j++){
+            if(nums[j]!=nums[i]){
+                i++;
+                nums[i]=nums[j];
+            }
+        }
+        return i+1;
+    }
+}
